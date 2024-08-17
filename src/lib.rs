68b790db17017938ff8b5f6376cc0d829ad19cea
Zk <zklim95@hotmail.com>
2024-08-18 04:26:20 +0800

    fix: uncomment

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-// #![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
 #[macro_use]
